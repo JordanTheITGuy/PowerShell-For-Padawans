33d42e77487d15d3eb6ecc4e1b79cb165a7991ea
Jordan Benzing <jordan.benzing@gmail.com>
2022-04-23 16:37:02 -0400

    Updates

---
+++ 1. Variables and Objects/Variabes-AndTypes.ps1
@@ -0,0 +1,25 @@
+# How to make a variable?
+
+$Var = "Hello World"
+
+# How to type cast a variable
+
+[string]$var2 = "Hello World"
+
+# Prove it, I don't believe you. 
+
+$var.GetType()
+$var2.GetType()
+
+#Fine but why do I care? 
+
+[string]$one = 1
+$two = 2
+
+$three = $one + $two 
+
+#Ok, but when am I ever going to accidentally do something like that... 
+
+Measure-Command{Get-Service | Where-Object {$_.Status -eq "Running"}}
+
+Measure-Command{Get-Service | Where-Object {$_.Status -eq [System.ServiceProcess.ServiceControllerStatus]::Running}}
